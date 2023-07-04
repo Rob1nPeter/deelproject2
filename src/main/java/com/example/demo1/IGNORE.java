326d1c75457935655ad9e74d46cb9399d2b21ef4
Robin <Robinpeterb@icloud.com>
2023-07-04 19:58:26 +0200

    ignore file delete

--- src/main/java/com/example/demo1/IGNORE.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo1;
-
-
-public class IGNORE {
-}
-
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.example.demo1 {
     requires javafx.fxml;
 
 
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1.templatemethod;
     opens com.example.demo1.templatemethod to javafx.fxml;
     exports com.example.demo1.App;
     opens com.example.demo1.App to javafx.fxml;
